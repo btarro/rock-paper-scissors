9b3c7b0ddcfea4441fbd2249e19178e9f755505d
Bruce Tarro <brucetarro@gmail.com>
2020-12-28 23:08:19 -0700

    Started working on game logic

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-// A Simple Rock Paper Scissors game
+// Rock, Paper, Scissors, Simple.
+
+// 
+
+
+var rock;
+var paper;
+var scissors;
+
+const gameObjects = ["rock", "paper", "scissors"];
+
+var playerValue = gameObjects[Math.floor(Math.random() * gameObjects.length)];
+
+var opponentValue = gameObjects[Math.floor(Math.random() * gameObjects.length)];
+
+console.log(playerValue);
+console.log(opponentValue);
+
+console.log(playerValue + opponentValue);
+
+// Next session - Let's combine to make win/lose/draw variables
+
